e2b24d1371ada3382bb656749db84942471becea
nuntawatrdd <67070088@kmitl.ac.th>
2026-09-05 16:36:01 +0700

    Change text in flask

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello Prodpai Cloud From EC2 #2"
+    return "hello Prodpai Cloud Create EC2 by ASG #1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080) # nosemgrep
