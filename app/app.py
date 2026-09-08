eadd78c66bf356377ecfbf62632ab7b42a524dcd
nuntawatrdd <67070088@kmitl.ac.th>
2026-09-08 23:58:50 +0700

    Health check path

--- app/app.py
+++ app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "hello Prodpai Cloud Create EC2 by ASG #1"
 
+@app.route('/health')
+def health_check():
+    return {"status": "healthy"}, 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080) # nosemgrep
