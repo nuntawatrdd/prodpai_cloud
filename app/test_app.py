107c95b84ccc5ec37e9b98f1cb2e5ca66ee30829
nuntawatrdd <67070088@kmitl.ac.th>
2026-09-09 10:15:31 +0700

    Wrong syntax

--- app/test_app.py
+++ app/test_app.py
@@ -1,20 +1,11 @@
 import pytest
 from app import app
 
-AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE1234567890" #key
-
 @pytest.fixture
-def client():
+def client()
     app.config.update({"TESTING": True})
     with app.test_client() as client:
         yield client
-
-# Command Injection
-@app.route("/ping")
-def ping():
-    host = request.args.get("host")
-    result = os.system("ping -c 1 " + host)
-    return str(result)
 
 def test_hello_route(client):
     response = client.get("/")
