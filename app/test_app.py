493034b4f5caac18bc090b24cd295da810fed1f0
nuntawatrdd <67070088@kmitl.ac.th>
2026-09-09 10:19:24 +0700

    Wrong function call

--- app/test_app.py
+++ app/test_app.py
@@ -2,7 +2,7 @@ import pytest
 from app import app
 
 @pytest.fixture
-def client()
+def cilent():
     app.config.update({"TESTING": True})
     with app.test_client() as client:
         yield client
