03360719f0ce8aab30a3b22a9553f8449f79c5cc
nuntawatrdd <67070088@kmitl.ac.th>
2026-09-09 10:00:52 +0700

    Semgrep Test

--- app/test_app.py
+++ app/test_app.py
@@ -1,11 +1,20 @@
 import pytest
 from app import app
 
+AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE1234567890" #key
+
 @pytest.fixture
 def client():
     app.config.update({"TESTING": True})
     with app.test_client() as client:
         yield client
+
+# Command Injection
+@app.route("/ping")
+def ping():
+    host = request.args.get("host")
+    result = os.system("ping -c 1 " + host)
+    return str(result)
 
 def test_hello_route(client):
     response = client.get("/")
